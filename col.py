fa49adfe85912caeda21ece58bfec7154ae90c4f
ConorFeeney <conor.feeney@student.fairfield.edu>
2017-10-17 16:38:48 -0400

    Finished

--- col.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def add_columns(dataframe,newcolname,names):
-    dataframe[newcolname]=0
-    for i in names:
-        dataframe[newcolname]=dataframe[newcolname]+dataframe[i]
-    return dataframe    
-    
