484b145a5ae8559b3b7412e67a8297efb5e8ce0c
ConorFeeney <conor.feeney@student.fairfield.edu>
2017-10-16 20:39:34 -0400

    update

--- Data.py
+++ Data.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-data = pd.read_csv('kaggleV2-May-2016.csv')
+data =pd.read_csv('KaggleV2-May-2016.csv',header=0,parse_dates=['ScheduledDay','AppointmentDay'],na_values='',names=['PatientId','AppointmentID','Gender','ScheduledDay','AppointmentDay','Age','Neighbourhood','Scholarship','Hypertension','Diabetes','Alcoholism','Disabled','SMS Received','No-show' ])
