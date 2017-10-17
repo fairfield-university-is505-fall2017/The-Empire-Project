871ba3d51741e669c36af43846ad779ce82570c8
ConorFeeney <conor.feeney@student.fairfield.edu>
2017-10-17 16:34:50 -0400

    Finished version

--- test_data.py
+++ test_data.py
@@ -2,9 +2,13 @@ import pandas as pd
 from Data import data
 import pytest
 
-@pytest.fixture
 
-def test_data():
+@pytest.fixture
+def thing():
+    return 1
+
+def test_data(thing):
     test=data
     assert all(test.columns ==['PatientId','AppointmentID','Gender','ScheduledDay','AppointmentDay','Age','Neighbourhood','Scholarship','Hypertension','Diabetes','Alcoholism','Disabled','SMS Received','No-show'])
     
+
